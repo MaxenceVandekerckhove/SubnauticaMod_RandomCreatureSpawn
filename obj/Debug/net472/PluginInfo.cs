e6beee8d03e143551d591c61067149dc576f8a3b
Maxence VANDEKERCKHOVE <maxence.vandekerckhove@viacesi.fr>
2025-02-22 15:37:20 +0100

    Removing /bin and /obj

--- obj/Debug/net472/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RandomCreatureSpawn
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "RandomCreatureSpawn";
-        public const string PLUGIN_NAME = "RandomCreatureSpawn";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
